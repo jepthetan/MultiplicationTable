81c35cc3a1b793d0aa0f72751c29994f9d1a3c19
JEPTHE TAN (DOCU-IRIS-ISD-OOCLL/MNL) <TANJE5@OOCL.COM>
2022-08-30 19:19:08 +0800

    push all directory files

---
+++ practice-multiplication-table/src/main/java/com/tw/MultiplicationTable.java
@@ -0,0 +1,33 @@
+package com.tw;
+
+public class MultiplicationTable {
+    public String create(int start, int end) {
+        return null;
+    }
+
+    public Boolean isValid(int start, int end) {
+        return null;
+    }
+
+    public Boolean isInRange(int number) {
+        return null;
+    }
+
+    public Boolean isStartNotBiggerThanEnd(int start, int end) {
+        return null;
+    }
+
+    public String generateTable(int start, int end) {
+        return null;
+    }
+
+    public String generateLine(int start, int row) {
+        return null;
+    }
+
+    public String generateSingleExpression(int multiplicand, int multiplier) {
+        int product = multiplicand * multiplier;
+        String expression = multiplicand + "*" + multiplier +  "=" + product;
+        return expression;
+    }
+}
